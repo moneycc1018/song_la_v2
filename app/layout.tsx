6e30f582258ca0dfdebacc590d3bfa19b52af7da
moneycc1018 <ccrr0101@gmail.com>
2025-11-27 16:39:50 +0800

    feat: initialize project
    20251127

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+
+import { Raleway, Roboto } from "next/font/google";
+
+import "../styles/globals.css";
+
+const roboto = Roboto({
+  weight: ["100", "300", "400", "500", "700", "900"],
+  subsets: ["latin"],
+  variable: "--font-roboto",
+});
+
+const raleway = Raleway({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+  variable: "--font-raleway",
+});
+
+export const metadata: Metadata = {
+  title: "Song La v2",
+  description: "A simple song game based on ytmusic.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${roboto.variable} ${raleway.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
